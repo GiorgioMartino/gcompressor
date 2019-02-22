997a9d83510de69f0d11ce779a15de57efb64f0a
Giorgio Martino <giorgio.martino.28@gmail.com>
2019-02-22 12:27:16 +0100

    Testing

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(name='gcompress',
       author='Giorgio Martino',
       author_email='giorgio.martino.28@gmail.com',
       url='https://github.com/GiorgioMartino',
-      # license='MIT',
       packages=['gcompress'],
      )
 
@@ -18,4 +17,8 @@ os.system("sudo cp ~/Università/compressor/gcomp /bin")
 os.system("sudo cp ~/Università/compressor/gdecomp /bin")
 
 if (sys.argv[1] == 'install'):
-    print('\n\n\nGCOMPRESS INSTALLED SUCCESSFULLY\n\n\n')
+    print('\n\nGCOMPRESS Package installed successfully.' +
+        '\n\n\t- gcomp\t\tFile and directory compressor using LZW.' +
+        '\n\t- gdecomp\tDecompressor for gcompself.' +
+        '\n\nSee --help for more informations.' +
+        '\nBUG Report: https://github.com/GiorgioMartino\n')
